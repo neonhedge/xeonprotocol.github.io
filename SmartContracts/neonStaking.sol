bfca26324697444924fbeb61d6099494d3112603
neonhedge <129759153+neonhedge@users.noreply.github.com>
2023-07-28 17:30:16 +0200

    Update neonStaking.sol
    Changed the reward mechanism to a simpler one using ethRewardBasis, ethLiquidityRewardBasis etc for different rewards. Added different claim functions for different rewards. Total assigned is tracked during assigning and assigning.

--- SmartContracts/neonStaking.sol
+++ SmartContracts/neonStaking.sol
@@ -2,16 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract StakingContract is Ownable {
     using SafeMath for uint256;
 
-    IERC20 public neonToken;
+    IERC20 public stakingToken;
     uint256 public stakingDuration = 30 days;
-    uint256 public rewardDistributionDay; // The day when the owner distributes rewards.
+    uint256 public poolExpiry; // The day when the staking pool expires and opens transacts for 3 days
     uint256 public nextUnstakeDay; // The day when stakers can unstake again.
+    uint256 public ethRewardBasis;
+    uint256 public ethLiquidityRewardBasis;
+    uint256 public ethCollateralRewardBasis ;
+    uint256 public totalAssignedForMining;
+    uint256 public totalAssignedForLiquidity;
+    uint256 public totalAssignedForCollateral;
 
     struct Staker {
         uint256 amount;
@@ -23,30 +29,36 @@ contract StakingContract is Ownable {
     }
 
     mapping(address => Staker) public stakers;
+    mapping(address => uint256) public lastRewardBasis;
+    mapping(address => uint256) public lastLiquidityRewardBasis;
+    mapping(address => uint256) public lastCollateralRewardBasis;
 
     event Staked(address indexed staker, uint256 amount);
     event Unstaked(address indexed staker, uint256 amount);
     event TokensAssigned(address indexed staker, uint256 amountForMining, uint256 amountForLiquidity, uint256 amountForCollateral);
     event TokensUnassigned(address indexed staker, uint256 amountFromMining, uint256 amountFromLiquidity, uint256 amountFromCollateral);
-    event RewardClaimed(address indexed staker, uint256 amount);
-    event RewardsDistributed(uint256 amount);
+    event RewardClaimed(address indexed staker, uint256 amount, uint indexed poolID);
+    event RewardsDistributed(uint256 amount, uint indexed poolID);
 
-    modifier stakingAllowed() {
-        require(block.timestamp >= nextUnstakeDay, "Staking is not allowed at the moment.");
+    modifier stakingWindow() {
+       require(block.timestamp >= nextUnstakeDay && block.timestamp <= nextUnstakeDay.add(3 days), "staking or assigning features suspended at the moment.");
         _;
     }
 
-    modifier claimAllowed() {
-        require(rewardDistributionDay > 0 && block.timestamp >= rewardDistributionDay.add(1 days), "Reward claim is not allowed at the moment.");
-        _;
+    constructor(address _stakingToken) {
+        require(_stakingToken != address(0), "Invalid token address.");
+        stakingToken = IERC20(_stakingToken);
     }
 
-    constructor(address _neonToken) {
-        require(_neonToken != address(0), "Invalid token address.");
-        neonToken = IERC20(_neonToken);
+    function startContract() onlyOwner {
+        nextUnstakeDay = block.timestamp;//3 days for people to stake and assign
     }
 
-    function stake(uint256 _amount) external stakingAllowed {
+    function restartPool() external stakingWindow onlyOwner {
+        nextUnstakeDay = block.timestamp.add(30 days);//pool 30 days start now
+    }
+
+    function stake(uint256 _amount) external stakingWindow {
         require(_amount > 0, "Staked amount must be greater than zero.");
         require(stakers[msg.sender].amount == 0, "You can only stake once at a time.");
 
@@ -64,22 +76,19 @@ contract StakingContract is Ownable {
         emit Staked(msg.sender, _amount);
     }
 
-    function unstake() external stakingAllowed {
+    function unstake() external stakingWindow {
         Staker storage staker = stakers[msg.sender];
         require(staker.amount > 0, "You have no staked tokens.");
 
-        uint256 stakingEndTime = staker.stakingTime.add(stakingDuration);
-        require(block.timestamp >= stakingEndTime, "You can't unstake until the stake matures.");
+        uint256 amountToUnstake = staker.amount.sub(staker.assignedForMining).sub(staker.assignedForLiquidity).sub(staker.assignedForCollateral);
+        staker.amount = staker.amount.sub(amountToUnstake);
 
-        uint256 amountToUnstake = staker.amount;
-        staker.amount = 0;
-
-        neonToken.transfer(msg.sender, amountToUnstake);
+        stakingToken.transfer(msg.sender, amountToUnstake);
 
         emit Unstaked(msg.sender, amountToUnstake);
     }
 
-    function assignTokens(uint256 _percentForMining, uint256 _percentForLiquidity, uint256 _percentForCollateral) external {
+    function assignTokens(uint256 _percentForMining, uint256 _percentForLiquidity, uint256 _percentForCollateral) external stakingWindow {
         Staker storage staker = stakers[msg.sender];
         require(staker.amount > 0, "You have no staked tokens.");
         require(_percentForMining.add(_percentForLiquidity).add(_percentForCollateral) <= 100, "Total assigned percentage cannot exceed 100%.");
@@ -100,10 +109,15 @@ contract StakingContract is Ownable {
         staker.assignedForLiquidity = staker.assignedForLiquidity.add(newAssignedForLiquidity);
         staker.assignedForCollateral = staker.assignedForCollateral.add(newAssignedForCollateral);
 
+        // Update globals
+        totalAssignedForMining = totalAssignedForMining.add(newAssignedForMining);
+        totalAssignedForLiquidity = totalAssignedForLiquidity.add(newAssignedForLiquidity);
+        totalAssignedForCollateral = totalAssignedForCollateral.add(newAssignedForCollateral);
+
         emit TokensAssigned(msg.sender, staker.assignedForMining, staker.assignedForLiquidity, staker.assignedForCollateral);
     }
 
-    function unassignTokens(uint256 _amountFromMining, uint256 _amountFromLiquidity, uint256 _amountFromCollateral) external {
+    function unassignTokens(uint256 _amountFromMining, uint256 _amountFromLiquidity, uint256 _amountFromCollateral) external stakingWindow {
         Staker storage staker = stakers[msg.sender];
         require(staker.amount > 0, "You have no staked tokens.");
 
@@ -117,67 +131,111 @@ contract StakingContract is Ownable {
         staker.assignedForLiquidity = staker.assignedForLiquidity.sub(_amountFromLiquidity);
         staker.assignedForCollateral = staker.assignedForCollateral.sub(_amountFromCollateral);
 
+        // Update globals
+        totalAssignedForMining = totalAssignedForMining.less(_amountFromMining);
+        totalAssignedForLiquidity = totalAssignedForLiquidity.less(_amountFromLiquidity);
+        totalAssignedForCollateral = totalAssignedForCollateral.less(_amountFromCollateral);
+
         emit TokensUnassigned(msg.sender, _amountFromMining, _amountFromLiquidity, _amountFromCollateral);
     }
 
     function depositRewards() external payable onlyOwner {
         require(msg.value > 0, "Reward amount must be greater than zero.");
-        emit RewardsDistributed(msg.value);
+        ethRewardBasis = ethRewardBasis.add(msg.value);
+        emit RewardsDistributed(msg.value, 1);
     }
 
-    function distributeRewards() external onlyOwner {
-        require(rewardDistributionDay == 0 || block.timestamp >= rewardDistributionDay.add(30 days), "You can distribute rewards once every 30 days.");
-
-        uint256 totalRewards = address(this).balance;
-        require(totalRewards > 0, "No rewards available to distribute.");
-
-        rewardDistributionDay = block.timestamp;
-        nextUnstakeDay = block.timestamp.add(30 days);
-
-        uint256 totalStaked = getTotalStaked();
-
-        for (address stakerAddress in getStakers()) {
-            Staker storage staker = stakers[stakerAddress];
-            uint256 stakerDays = block.timestamp.sub(staker.stakingTime).div(1 days);
-
-            // Calculate the staker's share of rewards based on the number of days staked.
-            uint256 stakerRewardShare = totalRewards.mul(staker.amount).mul(stakerDays).div(stakingDuration).div(totalStaked);
-
-            // Increment staker's lastClaimedDay to the reward distribution day.
-            staker.lastClaimedDay = rewardDistributionDay;
-
-            // Transfer the reward share to the staker's address.
-            payable(stakerAddress).transfer(stakerRewardShare);
-
-            emit RewardClaimed(stakerAddress, stakerRewardShare);
-        }
+    function depositLiquidityRewards() external payable onlyOwner {
+        require(msg.value > 0, "Reward amount must be greater than zero.");
+        ethLiquidityRewardBasis = ethLiquidityRewardBasis.add(msg.value);
+        emit RewardsDistributed(msg.value, 2);
     }
 
-    function claimRewards() external claimAllowed {
+    function depositCollateralRewards() external payable onlyOwner {
+        require(msg.value > 0, "Reward amount must be greater than zero.");
+        ethCollateralRewardBasis = ethCollateralRewardBasis.add(msg.value);
+        emit RewardsDistributed(msg.value, 3);
+    }
+
+    function claimRewards() external {
         Staker storage staker = stakers[msg.sender];
         require(staker.amount > 0, "You have no staked tokens.");
-        require(staker.lastClaimedDay < rewardDistributionDay, "You have already claimed your rewards for this distribution.");
-
-        uint256 stakerDays = block.timestamp.sub(staker.stakingTime).div(1 days);
-        uint256 stakerRewardShare = address(this).balance.mul(staker.amount).mul(stakerDays).div(stakingDuration).div(getTotalStaked());
+        
+        uint256 ethChange = ethRewardBasis - lastRewardBasis[msg.sender];
+        uint256 stakerRewardShare = ethChange.mul(staker.amount).div(getTotalStaked());
 
         staker.lastClaimedDay = rewardDistributionDay;
+        lastRewardBasis[msg.sender] = ethRewardBasis;
 
         payable(msg.sender).transfer(stakerRewardShare);
 
-        emit RewardClaimed(msg.sender, stakerRewardShare);
+        emit RewardClaimed(msg.sender, stakerRewardShare, 1);
+    }
+
+    function claimLiquidityRewards() external {
+        Staker storage staker = stakers[msg.sender];
+        require(staker.assignedForLiquidity > 0, "You have no tokens assigned for liquidity.");
+        
+        uint256 ethChange = ethLiquidityRewardBasis - lastLiquidityRewardBasis[msg.sender];
+        uint256 liquidityRewardShare = ethChange.mul(staker.assignedForLiquidity).div(totalAssignedForLiquidity);
+
+        staker.lastClaimedDay = block.timestamp;
+        lastLiquidityRewardBasis[msg.sender] = ethLiquidityRewardBasis;
+
+        payable(msg.sender).transfer(liquidityRewardShare);
+
+        emit RewardClaimed(msg.sender, liquidityRewardShare, 2);
+    }
+
+    function claimCollateralRewards() external {
+        Staker storage staker = stakers[msg.sender];
+        require(staker.assignedForCollateral > 0, "You have no tokens assigned for protocol collateral.");
+        
+        uint256 ethChange = ethCollateralRewardBasis - lastCollateralRewardBasis[msg.sender];
+        uint256 collateralRewardShare = ethChange.mul(staker.assignedForCollateral).div(totalAssignedForCollateral);
+
+        staker.lastClaimedDay = block.timestamp;
+        lastCollateralRewardBasis[msg.sender] = ethCollateralRewardBasis;
+
+        payable(msg.sender).transfer(collateralRewardShare);
+
+        emit RewardClaimed(msg.sender, collateralRewardShare, 3);
     }
 
     function getRewardsDue() external view returns (uint256) {
         Staker storage staker = stakers[msg.sender];
-        uint256 stakerDays = block.timestamp.sub(staker.stakingTime).div(1 days);
 
-        if (staker.amount == 0 || staker.lastClaimedDay >= rewardDistributionDay) {
+        if (staker.amount == 0) {
             return 0;
         }
 
-        uint256 stakerRewardShare = address(this).balance.mul(staker.amount).mul(stakerDays).div(stakingDuration).div(getTotalStaked());
+        uint256 ethChange = ethRewardBasis - lastRewardBasis[msg.sender];
+        uint256 stakerRewardShare = ethChange.mul(staker.amount).div(getTotalStaked());
         return stakerRewardShare;
+    }
+
+    function getLiquidityRewardsDue() external view returns (uint256) {
+        Staker storage staker = stakers[msg.sender];
+
+        if (staker.assignedForLiquidity == 0) {
+            return 0;
+        }
+
+        uint256 ethChange = ethLiquidityRewardBasis - lastLiquidityRewardBasis[msg.sender];
+        uint256 liquidityRewardShare = ethChange.mul(staker.assignedForLiquidity).div(totalAssignedForLiquidity);
+        return liquidityRewardShare;
+    }
+
+    function getCollateralRewardsDue() external view returns (uint256) {
+        Staker storage staker = stakers[msg.sender];
+
+        if (staker.assignedForCollateral == 0) {
+            return 0;
+        }
+
+        uint256 ethChange = ethCollateralRewardBasis - lastCollateralRewardBasis[msg.sender];
+        uint256 collateralRewardShare = ethChange.mul(staker.assignedForCollateral).div(totalAssignedForCollateral);
+        return collateralRewardShare;
     }
 
     function getAssignedAndUnassignedAmounts(address stakerAddress) external view returns (uint256 assignedForMining, uint256 assignedForLiquidity, uint256 assignedForCollateral, uint256 unassigned) {
@@ -193,7 +251,17 @@ contract StakingContract is Ownable {
     }
 
     function getTotalStaked() internal view returns (uint256) {
-        return neonToken.balanceOf(address(this));
+        return stakingToken.balanceOf(address(this));
+    }
+
+    function getTotalAssigned() public view returns (uint256) {
+        return totalAssignedForMining.add(totalAssignedForLiquidity).add(totalAssignedForCollateral);
+    }
+
+    function getTotalUnassigned() public view returns (uint256) {
+        uint256 totalStakedAmount = getTotalStaked();
+        uint256 totalAssignedAmount = getTotalAssigned();
+        return totalStakedAmount.sub(totalAssignedAmount);
     }
 
     function getStakers() internal view returns (address[] memory) {
