486377a0464665a6fb5b984e0c1f8fcad2f8d7a7
neonhedge <129759153+neonhedge@users.noreply.github.com>
2023-08-16 20:11:13 +0200

    Add files via upload
    Wallet Module added

---
+++ frontend/js/module-wallet-networth-dependencies.js
@@ -0,0 +1,60 @@
+// Function to fetch the number of deposited tokens by a wallet
+async function getWalletTokenList(walletAddress) {
+	try {
+	  const transactedTokensArray = await hedgingInstance.methods.getUserTokenList(walletAddress).call();
+	  return transactedTokensArray;
+	} catch (error) {
+	  console.error("Error fetching deposited tokens:", error);
+	  return [];
+	}
+}
+  
+// Function to calculate the total USD value of all token balances
+async function getCurrentBalancesValue(walletAddress) {
+	const transactedTokensArray = await getWalletTokenList(walletAddress);
+	let totalUSDValue = 0;
+	for (const underlyingTokenAddr of transactedTokensArray) {
+		const [deposited, withdrawn, , ,] = await hedgingInstance.methods.getuserTokenBalances(underlyingTokenAddr, walletAddress).call();
+		const ethUsdPrice = await getCurrentEthUsdcPriceFromUniswapV2();
+		// Convert deposited and withdrawn balances to BigNumber and handle 1e18 format
+		const depositedBalance = new BigNumber(deposited).div(new BigNumber(10).pow(18));
+		const withdrawnBalance = new BigNumber(withdrawn).div(new BigNumber(10).pow(18));
+		const currentBalance = depositedBalance.minus(withdrawnBalance);
+		// Get the USD value for the token balance
+		const usdValue = await getTokenUSDValue(underlyingTokenAddr, currentBalance, ethUsdPrice);
+		totalUSDValue = totalUSDValue.plus(usdValue);
+	}
+	return totalUSDValue;
+}
+
+// Function to calculate staked tokens value
+async function calculateStakedTokensValueETH(walletAddress) {
+	const stakedBalanceRaw = await stakingInstance.methods.getStakedBalance(walletAddress).call();
+	const stakedBalance = new BigNumber(stakedBalanceRaw).div(1e18);
+	const stakedTokensValueETH = await getTokenETHValue(CONSTANTS.wethAddress, stakedBalance);
+  
+	return stakedTokensValueETH;
+}
+
+// Function to calculate dividents due for claiming
+async function calculateRewardsDue(walletAddress, ethUsdPrice) {
+	const rewardsDue = await stakingInstance.methods.getRewardsDue().call({ from: walletAddress });
+	const rewardsDueETH = new BigNumber(rewardsDue).div(1e18);
+	const rewardsDueUSD = rewardsDueETH.times(ethUsdPrice);
+  
+	return [rewardsDueETH, rewardsDueUSD];
+}
+
+// Function to calculate commission
+async function calculateCommissionDueETH(walletAddress) {
+	const liquidityRewardsDue = await stakingInstance.methods.getLiquidityRewardsDue().call({ from: walletAddress });
+	const collateralRewardsDue = await stakingInstance.methods.getCollateralRewardsDue().call({ from: walletAddress });
+	const liquidityRewardsDueETH = new BigNumber(liquidityRewardsDue).div(1e18);
+	const collateralRewardsDueETH = new BigNumber(collateralRewardsDue).div(1e18);
+	const commissionDueETH = liquidityRewardsDueETH.plus(collateralRewardsDueETH);
+  
+	return [commissionDueETH, liquidityRewardsDueETH, collateralRewardsDueETH];
+}
+
+// Export the fetch functions
+export { getCurrentBalancesValue, calculateStakedTokensValueETH, calculateRewardsDue, calculateCommissionDueETH };
