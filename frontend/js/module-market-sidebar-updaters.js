1fd77dd122c269772aeaac789f409127fbeaa681
Neon Protocol <129759153+neonhedge@users.noreply.github.com>
2023-10-03 23:03:45 +0200

    Market volume updater  in place. More feature building to come to it

--- frontend/js/module-market-sidebar-updaters.js
+++ frontend/js/module-market-sidebar-updaters.js
@@ -1,26 +1,20 @@
 // 1. Update Sidebar Hedge Volumes 
 //----------------------------------------------------
 function updateSectionValues_volumes(hedgesTraded, hedgesCreated, swapsVolume, optionsVolume, settledVolume, hedgeCostsTotal, hedgeProfits, hedgeFees, cashierFees) {
-    // Format values
-    const formatValue = (value) => {
-      return `$${value.toFixed(2)}`;
-    };
-  
-    // Update hedges traded and created
-    document.getElementById("hedgesTraded").textContent = formatValue(hedgesTraded);
-    document.getElementById("hedgesCreated").textContent = formatValue(hedgesCreated);
-  
-    // Update swaps volume and options volume
-    document.getElementById("swapsVolume").textContent = formatValue(swapsVolume);
-    document.getElementById("optionsVolume").textContent = formatValue(optionsVolume);
-  
-    // Update hedge costs total
-    document.getElementById("hedgeCostsTotal").textContent = formatValue(hedgeCostsTotal);
-    document.getElementById("hedgeValueTotal").textContent = formatValue(hedgesTraded);
-  
-    // Update hedge profits and losses
-    document.getElementById("hedgeProfits").textContent = formatValue(hedgeProfits);
-    document.getElementById("hedgeFees").textContent = formatValue(hedgeFees);
-  }
+  // Format values
+  const formatValue = (value) => {
+    return `$${value.toFixed(2)}`;
+  };
 
-  export { updateSectionValues_volumes };
+  // Update hedges traded and created
+  document.getElementById("hedgeVolume").textContent = formatValue(hedgesTraded);
+  // Update hedge premiums
+  document.getElementById("premiumVolume").textContent = formatValue(hedgeCostsTotal);
+  // Update settled volume
+  document.getElementById("settleVolume").textContent = formatValue(settledVolume);  
+  // Update hedge profits and losses
+  document.getElementById("payoffVolume").textContent = formatValue(hedgeProfits);
+
+}
+
+export { updateSectionValues_volumes };
