37ca8072e1508aabe58f9323ef09755da4198a28
Neon Protocol <129759153+neonhedge@users.noreply.github.com>
2023-10-27 19:15:05 +0200

    Withdrawal module added to wallet. Cashier now distinct in function. Silkroad UX optimized again

--- frontend/js/_wallet.js
+++ frontend/js/_wallet.js
@@ -4,7 +4,7 @@
 import { CONSTANTS, isValidEthereumAddress, getUserBalancesForToken } from './constants.js';
 import { initWeb3 } from './dapp-web3-utils.js';
 import { unlockedWallet, reqConnect} from './web3-walletstatus-module.js';
-import { prepareDeposit, refreshBalances } from './module-wallet-writer.js';
+import { prepareDeposit, prepareWithdrawal, refreshBalances } from './module-wallet-writer.js';
 import { fetchSection_Networth, fetchSection_BalanceList, fetchSection_HedgePanel, fetchSection_RewardsPanel, fetchSection_StakingPanel } from './module-wallet-section-fetchers.js';
 import { loadHedgesModule } from './module-wallet-section-hedgesList.js';
 
@@ -167,10 +167,10 @@ document.querySelector('#cashierForm').addEventListener('submit', function(event
     console.log(values);
   
     // pass to setup deposits fr prep
-    setupDepositModule(values);
+    setupCashingModule(values);
 });
 
-export function setupDepositModule(formValues) {
+export function setupCashingModule(formValues) {
 
     console.log(formValues);
 
--- frontend/js/module-wallet-writer.js
+++ frontend/js/module-wallet-writer.js
@@ -71,6 +71,40 @@ async function prepareDeposit(tokenAddress, tokenAmount) {
             console.log('Insufficient funds to deposit.');
     }
 }
+
+
+async function prepareWithdrawal(tokenAddress, tokenAmount) {
+    // Retrieve wallet connected
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length === 0) {
+        console.log('No wallet connected. Please connect a wallet.');
+        return;
+    }
+    const walletAddress = accounts[0];
+
+    // ERC20 ABI
+    const erc20ABI = [ { constant: false, inputs: [ { name: '_spender', type: 'address', }, { name: '_value', type: 'uint256', }, ], name: 'approve', outputs: [ { name: 'success', type: 'bool', }, ], type: 'function', }, { constant: true, inputs: [ { name: '_owner', type: 'address', }, ], name: 'balanceOf', outputs: [ { name: 'balance', type: 'uint256', }, ], type: 'function', }, { constant: true, inputs: [], name: 'decimals', outputs: [ { name: '', type: 'uint8', }, ], type: 'function', }, { constant: true, inputs: [ { name: '_owner', type: 'address', }, { name: '_spender', type: 'address', }, ], name: 'allowance', outputs: [ { name: 'remaining', type: 'uint256', }, ], type: 'function', }, { constant: true, inputs: [], name: 'symbol', outputs: [ { name: '', type: 'string', }, ], type: 'function', }, ];
+
+    // Check wallet's token balance, already in wei
+    const tokenContract = new web3.eth.Contract(erc20ABI, tokenAddress);
+    const balance = await tokenContract.methods.balanceOf(walletAddress).call();
+
+    // Convert token amount to wei
+    const tokenDecimals = await tokenContract.methods.decimals().call();
+    const withdraw_amountWei = new BigNumber(tokenAmount.toString()).times(10 ** tokenDecimals);
+    const tokenSymbol = await tokenContract.methods.symbol().call();
+
+    if (withdraw_amountWei <= balance) {
+        
+        // Progress notification
+        tokenWithdrawSwal(tokenAddress, tokenAmount, tokenSymbol);
+        // Call proceed function
+        proceedWithdrawTx(tokenAddress, withdraw_amountWei, tokenSymbol);
+    
+    } else {
+            console.log('Insufficient funds to deposit.');
+    }
+}
   
 /* Depracated use of txHash to get receipt
 async function hasReceiptSuccess(txHash, callback) {
@@ -126,8 +160,8 @@ async function proceedDepositTx(tokenAddress, tokenAmount, tokenSymbol) {
                 console.log('Deposit status: '+receipt.status);console.log('Transaction hash:', receipt);
                 // Progress notification
                 tokenDepositSwal(tokenAddress, tokenAmount, tokenSymbol);
-                // Call proceedDepositTx function
-                proceedDepositTx(tokenAddress, deposit_amountWei, tokenSymbol);
+                // Call refresh function
+                refreshBalances();
             }
             else{
                 console.log('Deposit Failed Receipt status: '+receipt.status);
@@ -136,6 +170,50 @@ async function proceedDepositTx(tokenAddress, tokenAmount, tokenSymbol) {
         })
         .on('error', function (error) {
             console.error('Deposit error:', error);
+            swal({title: "Failed.",type: "error",allowOutsideClick: true,confirmButtonColor: "#F27474",text: "Transaction error: "+error});
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+async function proceedWithdrawTx(tokenAddress, tokenAmount, tokenSymbol) {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        if (accounts.length === 0) {
+            throw new Error('No wallet connected. Please connect a wallet.');
+        }
+        const walletAddress = accounts[0];
+
+        const functionSelector = hedgingInstance.methods.withdrawToken(tokenAddress, tokenAmount).encodeABI();
+    
+        const transactionObject = {
+            to: CONSTANTS.neonAddress,
+            data: functionSelector,
+            from: walletAddress
+        };
+    
+        const gasEstimate = await web3.eth.estimateGas(transactionObject);
+        transactionObject.gas = gasEstimate;
+    
+        // Submit withdraw Tx & Listen for the transaction to be mined
+        const receipt = await web3.eth.sendTransaction(transactionObject)
+        .on('receipt', function(receipt){
+            // Proceed to Call Withdraw Function now
+            if(receipt.status == true){//1 also matches true
+                console.log('Withdrawal status: '+receipt.status);console.log('Transaction hash:', receipt);
+                // Progress notification
+                tokenWithdrawSwal(tokenAddress, tokenAmount, tokenSymbol);
+                // Call refresh function
+                refreshBalances();
+            }
+            else{
+                console.log('Withdrawal Failed Receipt status: '+receipt.status);
+                swal({title: "Failed.",type: "error",allowOutsideClick: true,confirmButtonColor: "#F27474",text: "Transaction Failed Receipt status: "+receipt.status});
+            }
+        })
+        .on('error', function (error) {
+            console.error('Withdrawal error:', error);
             swal({title: "Failed.",type: "error",allowOutsideClick: true,confirmButtonColor: "#F27474",text: "Transaction error: "+error});
         });
     } catch (error) {
@@ -178,19 +256,19 @@ function tokenApprovalSwal(tokenAddress, amount, tokenSymbol) {
     });
 }
 
-  // Tx deposit request notification
+  // Tx deposit notification
 function tokenDepositSwal(tokenAddress, amount, tokenSymbol) {
     const privatize = `
     <div class="shl_inputshold delegate_inputshold setBeneField">
         <br>
         <div class="walletBalancesTL">
         <p>deposit tx pending... </p>
-        <span class="walletbalanceSpan">Depositing an amount of ${amount} ${tokenSymbol} to ${CONSTANTS.neonAddress}"></span></br>
+        <span class="walletbalanceSpan">Depositing an amount of ${amount} ${tokenSymbol} to Neon Vault ${CONSTANTS.neonAddress}"></span></br>
         </div>
     </div>`;
 
     swal({
-        title: "Depositing Tokens to Neon Vault",
+        title: "Depositing Tokens",
         text: privatize,
         type: "prompt",
         html: true,
@@ -206,6 +284,35 @@ function tokenDepositSwal(tokenAddress, amount, tokenSymbol) {
         
     });
 }
-  
+
+  // Tx withdrawal notification
+  function tokenWithdrawSwal(tokenAddress, amount, tokenSymbol) {
+    const privatize = `
+    <div class="shl_inputshold delegate_inputshold setBeneField">
+        <br>
+        <div class="walletBalancesTL">
+        <p>withdraw tx pending... </p>
+        <span class="walletbalanceSpan">Withdrawing an amount of ${amount} ${tokenSymbol} from Neon Vault ${CONSTANTS.neonAddress}"></span></br>
+        </div>
+    </div>`;
+
+    swal({
+        title: "Withdrawing Tokens",
+        text: privatize,
+        type: "prompt",
+        html: true,
+        dangerMode: true,
+        confirmButtonText: "Withdrawing",
+        confirmButtonColor: "#171716",
+        closeOnConfirm: false,
+        showLoaderOnConfirm: true,
+        showConfirmButton: true,
+        showCancelButton: false,
+        animation: "slide-from-top"
+    },async function(){//on confirm click
+        
+    });
+}
+
 export { prepareDeposit, refreshBalances }
   
