edadd23072d4892b6160e0b9fc17c68dbbf64212
Xeon Protocol <129759153+neonhedge@users.noreply.github.com>
2023-12-30 18:44:04 +0200

    Testnet Updates

--- frontend/js/module-wallet-section-hedgesList.js
+++ frontend/js/module-wallet-section-hedgesList.js
@@ -8,7 +8,7 @@ export async function loadHedgesModule(userAddress) {
   loadMoreButton.classList.add('load-more-button');
   loadMoreButton.style.display = 'none';
 
-  const hedgesList = document.querySelector('.hedges-trade-list');
+  const hedgesList = document.querySelector('#hedges-trade-list');
 
   async function fetchDataAndPopulateList() {
     let data;
@@ -36,49 +36,66 @@ export async function loadHedgesModule(userAddress) {
 
     const fragment = document.createDocumentFragment();
 
-    // For works better than foreach inside async function
-    for (const item of data) {
-      const result = await getHedgeDetails(item); 
-
-      const listItem = document.createElement('li');
-      listItem.classList.add('hedge-item');
-
-      const hedgeType = document.createElement('div');
-      hedgeType.classList.add('hedge-type', 'hedge-i-cat');
-      hedgeType.textContent = result.hedgeType;
-      listItem.appendChild(hedgeType);
-
-      const tokenInfo = document.createElement('div');
-      tokenInfo.classList.add('token-info');
-
-      const hedgeSymbol = document.createElement('div');
-      hedgeSymbol.classList.add('hedge-symbol', 'hedge-i-cat');
-      hedgeSymbol.textContent = result.hedgeSymbol;
-      tokenInfo.appendChild(hedgeSymbol);
-
-      const hedgeValue = document.createElement('div');
-      hedgeValue.classList.add('hedge-value', 'hedge-i-cat');
-      hedgeValue.textContent = result.hedgeValue; 
-      tokenInfo.appendChild(hedgeValue);
-
-      const tokenAddress = document.createElement('div');
-      tokenAddress.classList.add('token-address');
-      let truncatedAddr = tokenAddress.substring(0, 6) + '...' + tokenAddress.slice(-3);
-      tokenAddress.textContent = truncatedAddr; 
-      tokenInfo.appendChild(tokenAddress);
-
-      listItem.appendChild(tokenInfo);
-
-      const hedgeTxBtn = document.createElement('button');
-      hedgeTxBtn.classList.add('hedgeTxBtn');
-      hedgeTxBtn.textContent = 'Tx';
-      listItem.appendChild(hedgeTxBtn);
-
-      // Use fragments instead of adding elements one by one
-      fragment.appendChild(listItem);
+    if (data.length === 0 && startIndex > 0) {
+      // If data is empty, append a message to inform the user
+      const noHedgesMessage = document.createElement('div');
+      noHedgesMessage.textContent = 'no more hedges to load';
+      noHedgesMessage.classList.add('no-hedges-message');
+      hedgesList.appendChild(noHedgesMessage);
+    } else if (data.length === 0 && startIndex === 0) {
+      // Clear existing content in hedgesList
+      hedgesList.innerHTML = '';
+      const noHedgesMessage = document.createElement('div');
+      noHedgesMessage.textContent = 'No Hedges Found. Write or buy Options and Swaps to populate this area.';
+      noHedgesMessage.classList.add('no-hedges-message');
+      hedgesList.prepend(noHedgesMessage);
     }
+     else {
+      // If data is not empty, populate the list as before
+      // For works better than foreach inside async function
+      for (const item of data) {
+          const result = await getHedgeDetails(item); 
 
-    hedgesList.appendChild(fragment);
+          const listItem = document.createElement('li');
+          listItem.classList.add('hedge-item');
+
+          const hedgeType = document.createElement('div');
+          hedgeType.classList.add('hedge-type', 'hedge-i-cat');
+          hedgeType.textContent = result.hedgeType;
+          listItem.appendChild(hedgeType);
+
+          const tokenInfo = document.createElement('div');
+          tokenInfo.classList.add('token-info');
+
+          const hedgeSymbol = document.createElement('div');
+          hedgeSymbol.classList.add('hedge-symbol', 'hedge-i-cat');
+          hedgeSymbol.textContent = result.hedgeSymbol;
+          tokenInfo.appendChild(hedgeSymbol);
+
+          const hedgeValue = document.createElement('div');
+          hedgeValue.classList.add('hedge-value', 'hedge-i-cat');
+          hedgeValue.textContent = result.hedgeValue; 
+          tokenInfo.appendChild(hedgeValue);
+
+          const tokenAddress = document.createElement('div');
+          tokenAddress.classList.add('token-address');
+          let truncatedAddr = tokenAddress.substring(0, 6) + '...' + tokenAddress.slice(-3);
+          tokenAddress.textContent = truncatedAddr; 
+          tokenInfo.appendChild(tokenAddress);
+
+          listItem.appendChild(tokenInfo);
+
+          const hedgeTxBtn = document.createElement('button');
+          hedgeTxBtn.classList.add('hedgeTxBtn');
+          hedgeTxBtn.textContent = 'Tx';
+          listItem.appendChild(hedgeTxBtn);
+
+          // Use fragments instead of adding elements one by one
+          fragment.appendChild(listItem);
+      }
+      // Append the fragment to the list
+      hedgesList.appendChild(fragment);
+    }
 
     startIndex += data.length;
 
@@ -122,5 +139,5 @@ export async function loadHedgesModule(userAddress) {
     if (scrollPosition + windowHeight >= listHeight) {
       loadMoreButton.click();
     }
-  }, 300)); // Adjust delay (in milliseconds) for responsiveness
+  }, 15000)); // Adjust delay (in milliseconds) for responsiveness
 }
